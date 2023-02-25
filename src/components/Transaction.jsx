b77980f5274d3d1a67d1d69751acd920e800916a
goodness <goodyc474@gmail.com>
2023-02-25 23:17:45 +0100

    ft history

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className="transaction-list">
-      {transaction.text} <span>-$400</span>
+    <li
+      className={transaction.amount < 0 ? "minus" : "plus"}
+      id="transaction-list"
+    >
       <button className="delete-btn">x</button>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
     </li>
   );
 };
