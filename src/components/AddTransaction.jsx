8ce18cc85bd4e672ededcff399bbd9a5fbd04ba7
goodness <goodyc474@gmail.com>
2023-02-25 06:05:00 +0100

    add a state in form component

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amout, setAmount] = useState(0);
   return (
     <>
       <h3 className="add-new">Add New Transaction</h3>
       <form action="" className="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text" />
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text"
+            value={text}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="">
@@ -14,7 +21,12 @@ const AddTransaction = () => {
             <br />
             (negative-expense ,positive-income)
           </label>
-          <input type="text" placeholder="Enter amount" />
+          <input
+            type="text"
+            placeholder="Enter amount"
+            onChange={() => setAmount(e.target.value)}
+            value={amout}
+          />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
